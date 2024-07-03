3d87b1716f31dbe820da6c18a936fe44bfe29dab
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-07-03 12:38:21 -0500

    add evento

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,21 @@
-let tareas = []
-const lista_tareas = document.querySelector("#lista")
-const btn = document.querySelector("#agregar")
+let tareas = [];
+const lista_tareas = document.querySelector("#lista");
+const btn = document.querySelector("#agregar");
 
-const agregar_tareas = () =>{
+const agregar_tareas = () => {
+  let html = "";
+  for (const tarea of tareas) {
+    html += `<h4>${tarea}</h4>`;
+  }
 
+  lista_tareas.innerHTML = html;
+};
 
+agregar_tareas();
 
+btn.addEventListener("click", () => {
+  let nueva_tarea = document.querySelector("input").value;
+  nombres.push(nueva_tarea);
 
-    
-}
+  agregar_tareas();
+});
