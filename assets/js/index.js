10a52b23ada8acf85cb698d9ede93de896049396
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-07-03 19:31:05 -0500

    add tareas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
-let tareas = [];
+let tareas = [
+    {id: 301, nombre:"Asistir a clases", realizado: false,},
+    {id: 302, nombre:"Hacer la tarea", realizado: false,},
+    {id: 303, nombre:"Cocinar la cena", realizado: false,}
+];
 const lista_tareas = document.querySelector("#lista");
 const btn = document.querySelector("#agregar");
 const tarea_input =document.querySelector("#input")
@@ -6,7 +10,17 @@ const tarea_input =document.querySelector("#input")
 const agregar_tareas = () => {
   let html = "";
   for (const tarea of tareas) {
-    html += `<h4>${tarea}</h4>`;
+    html += `<div class="row">
+            <div class="col-2">${tarea.id}</div>
+            <div class="col-6">${tarea.nombre}</div>  
+          <div class="col-2"><input type='checkbox' onclick='actualizar(${
+            tarea.id
+          })' ${tarea.realizado === true ? "checked" : null}></div>
+          <div class="col-2"><button class="mb-1 btn btn-danger" onclick='eliminar(${
+            tarea.id
+          })'><i class="fa-solid fa-trash-can"></i></button></div> 
+          </div>
+          `;;
   }
 
   lista_tareas.innerHTML = html;
@@ -20,3 +34,16 @@ btn.addEventListener("click", () => {
   tarea_input.value = "";
   agregar_tareas();
 });
+
+
+const eliminar = (id)=>{
+
+let tareaindex = tareas.findIndex((tarea)=> tarea.id === id
+
+)
+
+tareas.splice(tareaindex,1)
+
+agregar_tareas()
+
+}
